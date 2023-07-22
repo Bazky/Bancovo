d72f4f473d55a318eb2f069c8dd0c5d0c321dc72
Adrian Nastał <adriannastal9@gmail.com>
2023-07-22 16:12:29 +0200

    app

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { clients } from "../constants";
+import styles from "../style";
 
-function Clients() {
-  return <div>Clients</div>;
-}
+const Clients = () => {
+  return (
+    <section className={`${styles.flexCenter} my-4`}>
+      <div className={`${styles.flexCenter} flex-wrap w-full`}>
+        {clients.map((client) => (
+          <div
+            key={client.id}
+            className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px]`}
+          >
+            <img
+              src={client.logo}
+              alt="client"
+              className="w-[100px] sm:w-[192px] object-contain"
+            />
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
 
 export default Clients;
